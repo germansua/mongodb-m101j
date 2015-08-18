2ba1a0a5b67a5764a5afdae09863c4432860dea8
German Suarez <German.Suarez@prodigious.com>
2015-08-18 11:33:06 -0500

    Final class

---
+++ Week2.3/Week2.3/src/main/java/co/gersua/mongo/week2/MainClass.java
@@ -0,0 +1,48 @@
+package co.gersua.mongo.week2;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Arrays.*;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        MongoClient client = new MongoClient();
+        MongoDatabase students = client.getDatabase("students");
+        MongoCollection<Document> grades = students.getCollection("grades");
+
+        Bson filter = Filters.eq("type", "homework");
+        Bson sort = Sorts.ascending(asList("student_id", "score"));
+
+        FindIterable<Document> resultDocuments = grades.find(filter).sort(sort);
+
+        Map<Integer, Double> studentMap = new HashMap<>();
+
+        for (Document doc : resultDocuments) {
+            Integer studentId = doc.get("student_id", Integer.class);
+            Double score = doc.get("score", Double.class);
+
+            if (!studentMap.containsKey(studentId)) {
+                studentMap.put(studentId, score);
+            }
+        }
+
+        System.out.println(studentMap.size());
+
+        for (Map.Entry<Integer, Double> entry : studentMap.entrySet()) {
+            Bson deleteFilter = Filters.and(Filters.eq("student_id", entry.getKey()), Filters.eq("score", entry.getValue()));
+            grades.deleteOne(deleteFilter);
+        }
+    }
+}
