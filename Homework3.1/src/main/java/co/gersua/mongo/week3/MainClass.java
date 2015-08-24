6aa8f0fe026bbb72df868aa0225ba774c33c9156
gersua <NewDeveloper2>
2015-08-24 00:20:49 -0500

    Some test code

--- Homework3.1/src/main/java/co/gersua/mongo/week3/MainClass.java
+++ Homework3.1/src/main/java/co/gersua/mongo/week3/MainClass.java
@@ -1,8 +1,18 @@
 package co.gersua.mongo.week3;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class MainClass {
 
     public static void main(String[] args) {
 
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase school = mongoClient.getDatabase("school");
+        MongoCollection<Document> students = school.getCollection("students");
+
+        System.out.println(students.count());
     }
 }
