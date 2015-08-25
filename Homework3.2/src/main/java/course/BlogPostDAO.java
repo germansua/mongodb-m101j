52ab676da8b95ab4b0c2633907f01ad673ba339a
gersua <NewDeveloper2>
2015-08-25 00:37:20 -0500

    Final Code

--- Homework3.2/src/main/java/course/BlogPostDAO.java
+++ Homework3.2/src/main/java/course/BlogPostDAO.java
@@ -1,9 +1,16 @@
 package course;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.*;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class BlogPostDAO {
@@ -17,10 +24,8 @@ public class BlogPostDAO {
     public Document findByPermalink(String permalink) {
 
         // XXX HW 3.2,  Work Here
-        Document post = null;
-
-
-
+        Bson findFilter = eq("permalink", permalink);
+        Document post = postsCollection.find(findFilter).first();
         return post;
     }
 
@@ -30,7 +35,13 @@ public class BlogPostDAO {
 
         // XXX HW 3.2,  Work Here
         // Return a list of DBObjects, each one a post from the posts collection
-        List<Document> posts = null;
+        List<Document> posts = new ArrayList<Document>();
+        Bson sortFilter = descending("date");
+        FindIterable<Document> result = postsCollection.find().sort(sortFilter).limit(limit);
+
+        for (Document document : result) {
+            posts.add(document);
+        }
 
         return posts;
     }
@@ -57,21 +68,20 @@ public class BlogPostDAO {
 
         // Build the post object and insert it
         Document post = new Document();
+        post.append("title", title);
+        post.append("author", username);
+        post.append("body", body);
+        post.append("permalink", permalink);
+        post.append("tags", tags);
+        post.append("comments", Collections.EMPTY_LIST);
+        post.append("date", new Date());
 
+        postsCollection.insertOne(post);
 
         return permalink;
     }
 
-
-
-
     // White space to protect the innocent
-
-
-
-
-
-
 
 
     // Append a comment to a blog post
@@ -83,5 +93,19 @@ public class BlogPostDAO {
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
         //   operator to append the comment on to any existing list of comments
-    }
+
+        Document postComment = new Document("author", name).append("body", body);
+        if (email != null) {
+            postComment.append("email", email);
+        }
+
+        Bson updateFilter = eq("permalink", permalink);
+        // Document post = postsCollection.find(updateFilter).first();
+        postsCollection.updateOne(updateFilter, new Document("$push", new Document("comments", postComment)));
+
+        /* Comment code approach is replacing all collection */
+        // List comments = post.get("comments", List.class);
+        // comments.add(postComment);
+        // postsCollection.updateOne(updateFilter, new Document("$set", new Document("comments", comments)));
+     }
 }
