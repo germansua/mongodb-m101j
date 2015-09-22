5f359ad5ef541f78ff12aa0869142c14951f6505
gersua <NewDeveloper2>
2015-09-21 22:46:28 -0500

    Final code

---
+++ Final.Q7/src/main/java/co/gersua/mongo/MainClass.java
@@ -0,0 +1,59 @@
+package co.gersua.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.in;
+
+/**
+ * Sanity check
+ *
+ * db.images.aggregate([
+ *      {"$unwind" : "$tags"},
+ *      {"$project" : {"_id" : 0, "tags" : 1}}
+ *      {"$match" : {"tags" : {"$eq" : "sunrises"}}},
+ *      {"$group" : { "_id" : "$tags", "count" : { "$sum" : 1 } } }
+ * ])
+ */
+
+
+public class MainClass {
+
+    public static void main(String[] args) throws Exception {
+
+        Set<Integer> deleteIndeces = new HashSet<>();
+
+        MongoClient client = new MongoClient();
+        MongoDatabase photoSharingDB = client.getDatabase("photo-sharing");
+
+        MongoCollection<Document> albums = photoSharingDB.getCollection("albums");
+        MongoCollection<Document> images = photoSharingDB.getCollection("images");
+
+        for (Document image : images.find()) {
+            int imageIndex = image.get("_id", Integer.class);
+
+            Bson filter = in("images", Arrays.asList(imageIndex));
+            FindIterable<Document> documents = albums.find(filter);
+            if (documents.iterator().hasNext()) {
+                continue;
+            } else {
+                deleteIndeces.add(imageIndex);
+            }
+        }
+
+        for (Integer index : deleteIndeces) {
+            images.deleteOne(eq("_id", index));
+        }
+
+        client.close();
+    }
+}
